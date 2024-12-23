86d9ce9d4d4a45f5b5dfe931fd61f29bb9a56579
dustij <knightlybyte@gmail.com>
2024-12-22 23:02:33 -0600

    tailwind config geist font variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-    return <div>hello world</div>;
+    return (
+        <div>
+            <h1 className="font-geist-sans">Portfolio</h1>
+            <h1 className="font-geist-mono">Portfolio</h1>
+        </div>
+    );
 }
