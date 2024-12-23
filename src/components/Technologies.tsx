7b21285735bc3003f667b701935f096e520e3c27
dustij <knightlybyte@gmail.com>
2024-12-23 13:34:56 -0600

    technologies

---
+++ src/components/Technologies.tsx
@@ -0,0 +1,32 @@
+import { FaJava, FaNodeJs, FaPython } from "react-icons/fa";
+import { GrMysql } from "react-icons/gr";
+import { RiReactjsLine } from "react-icons/ri";
+import { TbBrandNextjs } from "react-icons/tb";
+
+export default function Technologies() {
+    return (
+        <div className="pb-24">
+            <h2 className="my-20 text-center text-4xl">Technologies</h2>
+            <div className="flex flex-wrap items-center justify-center gap-4">
+                <div>
+                    <RiReactjsLine className="text-7xl text-cyan-400" />
+                </div>
+                <div className="p-4">
+                    <TbBrandNextjs className="text-7xl" />
+                </div>
+                <div className="p-4">
+                    <GrMysql className="text-7xl text-orange-400" />
+                </div>
+                <div className="p-4">
+                    <FaNodeJs className="text-7xl text-green-500" />
+                </div>
+                <div className="p-4">
+                    <FaJava className="text-7xl text-sky-500" />
+                </div>
+                <div className="p-4">
+                    <FaPython className="text-7xl text-yellow-300" />
+                </div>
+            </div>
+        </div>
+    );
+}
