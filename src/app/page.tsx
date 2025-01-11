8acb70220324f92efcdf23d11a2acbeeeb933023
dustij <knightlybyte@gmail.com>
2025-01-11 13:46:12 -0600

    Versioning untracked files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Hero from "@/components/hero";
-import Navbar from "@/components/nabar";
+import Hero from "~/components/hero";
+import Navbar from "~/components/nabar";
 
 export default function Home() {
   return (
-    <div className="mx-auto max-w-7xl bg-white">
+    <div className="bg-white max-w-7xl mx-auto">
       <Navbar />
       <Hero />
     </div>
