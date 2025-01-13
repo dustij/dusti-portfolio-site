13501cfcc158926a1a68a81cbd71fbf6064dd373
dustij <knightlybyte@gmail.com>
2025-01-13 02:40:25 -0600

    fixed typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default async function Home() {
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             Hey there, I'm Dusti&mdash;a software engineer and computer science
-            student. Usually, I'm buring the midnight oil, working on a personal
+            student. Usually, I'm burning the midnight oil, working on a personal
             project or diving into documentation.
           </p>
           <div className="mt-6 flex gap-6">
